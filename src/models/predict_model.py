51f471124d5843d83e9ee5c7b7147a920daa0580
rafa <rkchelebiev@gmail.com>
2022-05-12 01:42:52 +0300

    Add dvc metrics tracking

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,3 +1,4 @@
+import json
 import click
 import pickle
 import pandas as pd
@@ -6,6 +7,10 @@ import seaborn as sns
 from pathlib import Path
 from matplotlib import pyplot as plt
 
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import median_absolute_error
+
 from src.params_file import PARAMS_FILE
 from src.utility.processing import load_json_params
 
@@ -13,8 +18,9 @@ from src.utility.processing import load_json_params
 @click.command()
 @click.argument("input_model", type=click.Path(exists=True))
 @click.argument("input_data", type=click.Path(exists=True))
+@click.argument("output_metrics", type=click.Path())
 # @click.argument('y_true', type=click.Path(exists=True), default='')
-def predict(input_model: str, input_data: str):
+def predict(input_model: str, input_data: str, output_metrics: str):
     params = load_json_params(PARAMS_FILE)
     Y_COLUMN = params['Y_COLUMN']
     REPORT_PATH = Path(params['REPORT_PATH'])
@@ -29,6 +35,14 @@ def predict(input_model: str, input_data: str):
     x = df.drop(labels=[Y_COLUMN], axis=1)
 
     y_pred = model.predict(x)
+    scores = dict(
+        mean_absolute_error=mean_absolute_error(y_true, y_pred),
+        median_absolute_error=median_absolute_error(y_true, y_pred),
+        r2_score=r2_score(y_true, y_pred)
+    )
+
+    with open(output_metrics, 'w') as file:
+        json.dump(scores, file, indent=4)
 
     plt.figure(figsize=(12, 7))
     sns.histplot(y_true - y_pred, color="teal", label=model_name)
